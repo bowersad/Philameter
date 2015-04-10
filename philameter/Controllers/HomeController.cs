df4a472b46d33db78f7db2d2289fccc9f0372522
bowersad <bowersad@gmail.com>
2015-04-09 21:29:33 -0400

    service layer refactor

--- philameter/Controllers/HomeController.cs
+++ philameter/Controllers/HomeController.cs
@@ -4,14 +4,28 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using philameter.DAL;
+using philameter.DAL.Entities;
+using philameter.DAL.Repositories;
+
 namespace philameter.Controllers
 {
     public class HomeController : Controller
     {
+        private PhilaModelService  service;
+
+        public HomeController()
+        {
+            service = new PhilaModelService();
+        }
+        
+
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var StatsModel = service.Stats.Get();
+
+            return View(StatsModel);
         }
 
         public ActionResult About()
