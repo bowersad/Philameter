5e027c20e446e805585f1892944b752acb240cc6
bowersad <bowersad@gmail.com>
2017-10-24 20:35:20 -0400

    Wiring new dashboard to API

--- philameter/Controllers/HomeController.cs
+++ philameter/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 using philameter.DAL;
 using philameter.DAL.Entities;
 using philameter.DAL.Repositories;
+using RestSharp;
 
 
 namespace philameter.Controllers
@@ -31,7 +32,36 @@ namespace philameter.Controllers
         [Route("/Home/NewDash")]
         public  ActionResult NewDash()
         {
-            var StatsModel = service.Stats.Get();
+            var client = new RestClient("http://api.philameter.com");
+            // client.Authenticator = new HttpBasicAuthenticator(username, password);
+
+            var request = new RestRequest("/Stats", Method.GET);
+            //request.AddParameter("name", "value"); // adds to POST or URL querystring based on Method
+            //request.AddUrlSegment("id", "123"); // replaces matching token in request.Resource
+
+            // easily add HTTP Headers
+            //request.AddHeader("header", "value");
+
+            // add files to upload (works with compatible verbs)
+            ///request.AddFile(path);
+
+            // execute the request
+            //IRestResponse response = client.Execute(request);
+            //var content = response.Content; // raw content as string
+
+            // or automatically deserialize result
+            // return content type is sniffed but can be explicitly set via RestClient.AddHandler();
+            //List<Stats> StatsModel;
+            var response2 = client.Execute<List<Stats>>(request);
+            var StatsModel = response2.Data;
+
+            // easy async support
+            //client.ExecuteAsync(request, response => {
+            //    Console.WriteLine(response.Content);
+            //});
+
+            //var StatsModel = service.Stats.Get();
+            //StatsModel = response2;
 
             return View(StatsModel);
         }
